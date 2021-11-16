e878a334c4f07b2db8093fd9147fabeac830a33c
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2021-11-16 07:04:42 +0200

    Add: BetterPrint.swift

---
+++ misc/BetterPrint.swift
@@ -0,0 +1,29 @@
+/*
+ * This a functions shadowing the Swift.print function for better logging
+ * 
+ * I learned it from: https://stackoverflow.com/a/59576554/4538920
+ */
+
+// ref: https://stackoverflow.com/a/59576554/4538920
+
+public func print(_ items: String..., filename: String = #file, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n") {
+    #if DEBUG
+        let pretty = "\(URL(fileURLWithPath: filename).lastPathComponent) [#\(line)] \(function)\n\t-> "
+        let output = items.map { "\($0)" }.joined(separator: separator)
+        Swift.print(pretty+output, terminator: terminator)
+    
+    #else
+        Swift.print("RELEASE MODE")
+    #endif
+}
+
+// Support dictionary and array printing
+public func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    #if DEBUG
+        let output = items.map { "\($0)" }.joined(separator: separator)
+        Swift.print(output, terminator: terminator)
+    
+    #else
+        Swift.print("RELEASE MODE")
+    #endif
+}
